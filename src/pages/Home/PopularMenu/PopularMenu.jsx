ac5dc731d077ce5d048e2abf62b2fc871caabf98
rumankazi62 <rumankazi629@gmail.com>
2023-08-31 12:43:17 +0600

    first commit

---
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
+
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle heading={'FROM OUR MENU'} subHeading={'Check it out'}></SectionTitle>
+    </section>
+  );
+};
+
+export default PopularMenu;
